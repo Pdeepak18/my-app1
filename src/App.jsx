c99f68878f8ada47b08fdee2dcf845b55baacfc9
Pdeepak18 <85224426+Pdeepak18@users.noreply.github.com>
2023-04-04 11:58:06 +0530

    React + Tailwind starter Pack

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=' text-2xl bg-gray-500 text-center'>Hello World !!! , Tailwind + CSS</h1>
+      
+    </div>
+  );
+}
+
+export default App;
